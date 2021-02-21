1db1f86766fa41636b0c6e392d0c9c0784e2d0fe
solvencino <harshparamar84@gmail.com>
2021-02-21 13:46:39 +0530

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  purge: ['./src/**/*.html'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        lightblue: {
+          DEFAULT: '#9DDEFF',
+        },
+        dark: {
+          light: '#282828',
+          DEFAULT: '#181818',
+        },
+      },
+      margin: {
+        70: '17.58rem',
+      },
+      keyframes: {
+        'bounce-fast': {
+          '0%, 100%': { transform: 'translateY(3px)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+      },
+      animation: {
+        'bounce-fast': 'bounce-fast 1s infinite',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
